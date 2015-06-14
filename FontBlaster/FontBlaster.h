9e9322f2603a3bf5865570a92b0b3f86cc51d861
Kasper Lahti <kasper@soundcloud.com>
2015-06-14 15:22:05 +0200

    Carthage support

---
+++ FontBlaster/FontBlaster.h
@@ -0,0 +1,19 @@
+//
+//  FontBlaster.h
+//  FontBlaster
+//
+//  Created by Kasper Lahti on 6/14/15.
+//  Copyright (c) 2015 Arthur Ariel Sabintsev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FontBlaster.
+FOUNDATION_EXPORT double FontBlasterVersionNumber;
+
+//! Project version string for FontBlaster.
+FOUNDATION_EXPORT const unsigned char FontBlasterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontBlaster/PublicHeader.h>
+
+
