1d599ee22d2f9d2d7be5c1df131bdb3e6ff0ac90
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2019-08-07 21:41:26 -0400

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-  name: "FontBlaster"
+    name: "Siren",
+    products: [.library(name: "FontBlaster", targets: ["FontBlaster"])],
+    targets: [.target(name: "FontBlaster", path: "Sources")]
 )
