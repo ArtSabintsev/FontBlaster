a75d37bc9a3cbb2513b918f701321050c827ca96
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2020-06-12 14:50:42 -0400

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,8 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "FontBlaster",
     products: [.library(name: "FontBlaster", targets: ["FontBlaster"])],
-    targets: [.target(name: "FontBlaster", path: "Sources")]
+    targets: [.target(name: "FontBlaster", path: "Sources")],
+    platforms: [.iOS(.v8)],  
+    swiftLanguageVersions: [.v5]
 )
