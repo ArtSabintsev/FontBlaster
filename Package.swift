1f0856f10a7ce3821e36236ee597bdbacce049bc
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2020-06-13 15:35:23 -0400

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "FontBlaster",
-    platforms: [.iOS(.v8)],
+    platforms: [.iOS(.v8), .tvOS(.v9)],
     products: [.library(name: "FontBlaster", targets: ["FontBlaster"])],
     targets: [.target(name: "FontBlaster", path: "Sources")],  
     swiftLanguageVersions: [.v5]
